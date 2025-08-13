85436682513e5ed772109613259235cb913bb4a6
Lkkkkky <1158356001@qq.com>
2025-08-13 14:06:02 +0800

    update-增加重试机制

--- dags/xhs_dags/xhs_account_name.py
+++ dags/xhs_dags/xhs_account_name.py
@@ -97,5 +97,6 @@ with DAG(
                 'device_index': index,
             },
             provide_context=True
-                   
+            retries=3,
+            retry_delay=timedelta(seconds=10),
         )
